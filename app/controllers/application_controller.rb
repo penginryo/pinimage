ea99aa5a5acd68906b53135871efaca0cfb8203e
penginryo <penginryo@gmail.com>
2015-08-19 18:31:39 -0700

    add profile picture registration feature to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :username, :password, :password_confirmation)}
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:avatar, :email, :username, :password, :password_confirmation)}
       devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password, :remember_me)}
-      devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation, :admin)}
+      devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:avatar, :username, :email, :password, :password_confirmation, :admin)}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811025712) do
+ActiveRecord::Schema.define(version: 20150819025812) do
 
   create_table "pins", force: true do |t|
     t.string   "title"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20150811025712) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
