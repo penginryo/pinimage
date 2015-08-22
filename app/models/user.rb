f2e05536ba4105956e1357e74c407a2d31c40c21
penginryo <penginryo@gmail.com>
2015-08-21 19:57:08 -0700

    add default user profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100" }
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
+  # def update_without_current_password(params, *options)
+  # 	params.delete(:current_password)
+
+  # 	if params[:password].blank?
+  # 		params.delete(:password)
+  # 		params.delete(:password_confirmation) if params[:password_confirmation].blank?
+  # 	end
+
+  # 	clean_up_passwords
+  # 	update_attributes(params, *options)
+  # end
+
 end
