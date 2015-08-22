4e1dc6ec566c55067d84655c4ff7c60c0e8712c4
penginryo <penginryo@gmail.com>
2015-08-21 20:11:36 -0700

    fix a bug not showing profile picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:avatar, :email, :username, :password, :password_confirmation)}
       devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :password, :remember_me)}
-      devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:password, :password_confirmation, :current_password)}
+      devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:avatar, :password, :password_confirmation, :current_password)}
   end
 end
